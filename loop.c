80267318fee48345307654fd805afc874b444e9e
LucJamal <142950269+LucJamal@users.noreply.github.com>
2024-12-12 10:18:13 -0300

    Create loop.c
    Código em C para rodar o .bat

---
+++ loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main() {
+    while (1) { // Loop infinito
+        system("C:\\Users\\adr_robo\\Desktop\\teste.bat");
+        Sleep(60000);
+    }
+    return 0;
+}
